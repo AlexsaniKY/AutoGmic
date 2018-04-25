a429c84701a931b093ceac9c69f468e226426b73
AlexsaniKY <cerafem@gmail.com>
2018-04-25 16:53:01 -0400

    added notes to drive script development

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
+import subprocess, os
 
+print __file__
+print os.getcwd()
 
 if __name__ == "__main__":
-	subprocess.call(["gmic","x_shadebobs"])
+	#subprocess.call(["gmic","x_shadebobs"])
 	pass
