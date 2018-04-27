2f8faef3e6abd6271ba30cc0e136ae3bd73d1b35
AlexsaniKY <cerafem@gmail.com>
2018-04-27 18:56:56 -0400

    added notes for directory information

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess, os
 
-print __file__
-print os.getcwd()
+print(__file__)#this file's directory
+print(os.getcwd())#the directory this file was called from
 
 if __name__ == "__main__":
 	#subprocess.call(["gmic","x_shadebobs"])
