c4afc0c6643f5546312e65b988709a0d82a28eeb
AlexsaniKY <cerafem@gmail.com>
2018-04-28 17:45:22 -0400

    encapsulated log stripping into function

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import subprocess, os
 print(__file__)#this file's directory
 print(os.getcwd())#the directory this file was called from
 
+def open_log(): pass
+
+def filter_commands(input):
+	commands = []
+	for line in input:
+		statements = str.split(line)
+		if len(statements)==6:
+			if statements[1] == 'Command:' and statements[4][-8:] != '_preview':
+				commands.append(statements[-2:])
+	return commands
+
 if __name__ == "__main__":
 	#subprocess.call(["gmic","x_shadebobs"])
 	if os.name is 'nt':
@@ -10,9 +21,9 @@ if __name__ == "__main__":
 	print(gmic_log)
 	commands = []
 	with open(gmic_log, 'r') as f:
-		for line in f:
-			statements = str.split(line)
-			if len(statements)==6:
-				if statements[1] == 'Command:' and statements[4][-8:] != '_preview':
-					print(statements[-2:])
+		print(filter_commands(line for line in f))
+			# statements = str.split(line)
+			# if len(statements)==6:
+				# if statements[1] == 'Command:' and statements[4][-8:] != '_preview':
+					# print(statements[-2:])
 	pass
