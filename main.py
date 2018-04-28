6148a7103b57198138ec48ea1bc6ed4a7095a5e8
AlexsaniKY <cerafem@gmail.com>
2018-04-28 13:45:22 -0400

    added filtering for applied commands

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ print(os.getcwd())#the directory this file was called from
 
 if __name__ == "__main__":
 	#subprocess.call(["gmic","x_shadebobs"])
+	if os.name is 'nt':
+		gmic_log = "".join( (os.environ['HOME'], r'\AppData\Roaming\gmic\gmic_qt_log')) 
+	print(gmic_log)
+	commands = []
+	with open(gmic_log, 'r') as f:
+		for line in f:
+			statements = str.split(line)
+			if len(statements)==6:
+				if statements[1] == 'Command:' and statements[4][-8:] != '_preview':
+					print(statements[-2:])
 	pass
