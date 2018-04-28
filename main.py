c73aa3bdfd609f155c6861591b7dac5710742d5a
AlexsaniKY <cerafem@gmail.com>
2018-04-28 19:16:14 -0400

    encapsulated log locating and opening

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import subprocess, os
 print(__file__)#this file's directory
 print(os.getcwd())#the directory this file was called from
 
-def open_log(): pass
+def open_log(): 
+	if os.name is 'nt':
+		gmic_log = "".join( (os.environ['HOME'], r'\AppData\Roaming\gmic\gmic_qt_log'))
+	return open(gmic_log, 'r')
 
 def filter_commands(input):
 	commands = []
@@ -16,14 +19,6 @@ def filter_commands(input):
 
 if __name__ == "__main__":
 	#subprocess.call(["gmic","x_shadebobs"])
-	if os.name is 'nt':
-		gmic_log = "".join( (os.environ['HOME'], r'\AppData\Roaming\gmic\gmic_qt_log')) 
-	print(gmic_log)
 	commands = []
-	with open(gmic_log, 'r') as f:
+	with open_log() as f:
 		print(filter_commands(line for line in f))
-			# statements = str.split(line)
-			# if len(statements)==6:
-				# if statements[1] == 'Command:' and statements[4][-8:] != '_preview':
-					# print(statements[-2:])
-	pass
