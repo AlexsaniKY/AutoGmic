957f0fa46760d7c832b834e33eb7ef144c37a1df
AlexsaniKY <cerafem@gmail.com>
2018-04-29 18:04:37 -0400

    input walking commit, needs to walk from child to parent

--- main.py
+++ main.py
@@ -36,13 +36,39 @@ def capture(subset = None):
 				command_file.write(subset)
 			command_file.write('\n')
 			
-	# subsets_path = "".join((os.getcwd(), r'\subsets.txt'))
-	# with open(subsets_path, 'a'):
-		# if(subset is None):
-		
+def add_subset(subset, file_names):
+	#register subset
+	subsets_registry_path = "".join((os.getcwd(), r'\subsets.txt'))
+	with open(subsets_registry_path, 'a') as reg_file:
+		reg_file.write(subset)
+		reg_file.write('\n')
+	
+	#store subset filenames
+	new_subset_path = "".join((os.getcwd(), r'\subsets\\', subset))
+	with open(new_subset_path, 'a') as subset_file:
+		for file in file_names:
+			subset_file.write(file)
+			subset_file.write("\n")
+			
+def walk_input_directory():
+	directories = {}
+	sets = {}
+	#each folder is a key in sets, and its subdirectories are saved in directories
+	for path, names, files in os.walk("".join((os.getcwd(), r'\input'))):
+		this_folder = path.split("\\")[-1]
+		if names:
+			directories[this_folder] = names
+		sets[this_folder] = files
+	#NEEDS TO ITERATE BACKWARDS
+	for parent, subfolders in directories.items():
+		sets[parent].extend(sets[subfolders])
+	print(directories)
+	print(sets)
 
 if __name__ == "__main__":
 	#subprocess.call(["gmic","x_shadebobs"])
 	if 'capture' in sys.argv:
 		capture()
+	else:
+		walk_input_directory()
 		
